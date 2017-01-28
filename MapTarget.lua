d160aec1d60d392bad21c6f3a2843655cfcd4df5
EinBaum <mail@einbaum.org>
2017-01-28 23:40:51 +0100

    Initial

---
+++ MapTarget.lua
@@ -0,0 +1,30 @@
+
+local click_orig = nil
+
+local click_target = function()
+	local name = getglobal("GameTooltipTextLeft1"):GetText()
+	if name then
+		TargetByName(name)
+	end
+end
+
+local click_new = function()
+	if IsShiftKeyDown() then
+		click_orig()
+	else
+		click_target()
+	end
+end
+
+local setup = function()
+	click_orig = Minimap_OnClick
+	Minimap_OnClick = click_new
+end
+
+local f = CreateFrame("frame")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:SetScript("OnEvent", function()
+	if event == "PLAYER_ENTERING_WORLD" then
+		setup()
+	end
+end)
