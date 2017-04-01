266f438ebf995eca11b60085da2c00edc107cb78
EinBaum <mail@einbaum.org>
2017-04-01 20:02:52 +0200

    1. Fix targetting inside buildings 2. Pressing shift increases ping range by 5 times

--- MapTarget.lua
+++ MapTarget.lua
@@ -1,16 +1,48 @@
 
 local click_orig = nil
 
+local escapes = {
+    ["|c%x%x%x%x%x%x%x%x"] = "", -- color start
+    ["|r"] = "", -- color end
+    ["|H.-|h(.-)|h"] = "%1", -- links
+    ["|T.-|t"] = "", -- textures
+}
+local function unescape(str)
+    for k, v in pairs(escapes) do
+        str = gsub(str, k, v)
+    end
+    return str
+end
+
 local click_target = function()
 	local name = getglobal("GameTooltipTextLeft1"):GetText()
 	if name then
+		name = unescape(name)
 		TargetByName(name)
 	end
 end
 
 local click_new = function()
-	if SpellIsTargeting() or IsShiftKeyDown() or IsControlKeyDown() or IsAltKeyDown() then
-		click_orig()
+	local shift = IsShiftKeyDown()
+	if SpellIsTargeting() or shift or IsControlKeyDown() or IsAltKeyDown() then
+		local x, y = GetCursorPosition()
+		x = x / this:GetEffectiveScale()
+		y = y / this:GetEffectiveScale()
+
+		local cx, cy = this:GetCenter()
+		x = x - cx
+		y = y - cy
+
+		if shift then
+			x = x * 5
+			y = y * 5
+		end
+
+		x = x + CURSOR_OFFSET_X
+		y = y + CURSOR_OFFSET_Y
+
+		Minimap:PingLocation(x, y)
+		--DEFAULT_CHAT_FRAME:AddMessage("Ping: " .. x .. " " .. y)
 	else
 		click_target()
 	end
